f32754cfa707e3df13478a829efa49467bcfee69
Erick Suarez <ericksuarezdev@gmail.com>
2019-12-05 22:06:47 -0600

    Structure project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,54 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+//Modulos
+import { AppRountingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatSliderModule } from '@angular/material/slider'
+import { MatSliderModule } from '@angular/material/slider';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
+import { SidebarComponent } from './shared/components/sidebar/sidebar.component';
+import { IndexComponent } from './index/index.component';
+import { LedgerComponent } from './ledger/ledger.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,    
+    LoginComponent,
+    RegisterComponent,
+    DashboardComponent,
+    FooterComponent,
+    NavbarComponent,
+    SidebarComponent,
+    IndexComponent,
+    LedgerComponent,
   ],
   imports: [
     MatSliderModule,
     BrowserModule,
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    AppRountingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
